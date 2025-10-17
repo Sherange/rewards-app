89cefb6e66d73499983afbca3603f6e1fd0924f3
Sherange Fonseka <sherangef@gmail.com>
2025-10-18 00:53:14 +0530

    add home screen

---
+++ src/screens/Home.tsx
@@ -0,0 +1,22 @@
+import { View, StyleSheet, Text } from 'react-native';
+const Home = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Home</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#FFFFFF',
+  },
+  text: {
+    color: '#000000',
+  },
+});
+
+export default Home;
